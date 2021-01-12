f9d97eac4ecc93f10a23a06c5cc98353f9b774a9
JasonGarner7 <jason.garner7@gmail.com>
2021-01-12 16:49:10 -0700

    beginning to flesh out assignments page

--- src/Components/Assignments.js
+++ src/Components/Assignments.js
@@ -1,17 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CreateAssignment from './CreateAssignment'
 import './css/Assignments.css'
+import Files from './Files'
 
 function Assignments(props){
+    const [showContent, setShowContent] = useState(false)
+    const [showCreateDialog, setShowCreateDialog] = useState(false)
+
+    let content = <div className='assignment-list'>Assignment List...</div>
+    if(showContent){
+        content = <Files goBack={() => setShowContent(false)}/>
+    }
+
+    function cancel() {
+        setShowContent(false)
+        setShowCreateDialog(false)
+    }
+    let createDialog
+    let createDialogMask
+    if(showCreateDialog){
+        createDialog = <CreateAssignment goBack={cancel}/>
+        createDialogMask = <div className='dialog-mask' />
+    }
+
     return (
         <div className='assignments'>
             <div className='side-panel'>
-                <div className='side-panel-item'>
+                <div 
+                    className='side-panel-item' 
+                    onClick={ () => setShowCreateDialog(true) }
+                >
                     Create Assignment +
                 </div>
-                <div className='side-panel-item'>
+                <div className='side-panel-item' onClick={ () => setShowContent(true) }>
                     Files
                 </div>
             </div>
+            <div>
+                {content}
+            </div>
+            {createDialogMask}
+            {createDialog}
         </div>
 
     );
---
+++ src/Components/CreateAssignment.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './css/CreateAssignment.css'
+
+function CreateAssignment(props){
+    return (
+        <div className='dialog'>
+            <div className='title'>
+                New Assignment 1
+            </div>
+            <div className='add-problems'>
+                + Add a problem
+            </div>
+            <div className='cancel' onClick={props.goBack}>
+                Cancel
+            </div>
+        </div>
+    );
+}
+
+export default CreateAssignment;
