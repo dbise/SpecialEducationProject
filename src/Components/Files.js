2a9c0e57bb256d73724b66e1a9ba7aeff80655e0
JasonGarner7 <jason.garner7@gmail.com>
2021-01-12 16:49:10 -0700

    beginning to felsh out assignments page

---
+++ src/Components/Files.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './css/Files.css'
+
+function Files(props){
+    return (
+        <div className='files'>
+            <div className='go-back' onClick={props.goBack}>
+                &lt; Back to Assignments
+            </div>
+            <div className='files-content'>
+                Files...
+            </div>
+        </div>
+    );
+}
+
+export default Files;
