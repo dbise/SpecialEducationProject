a8cf58e2d7ad8be5f4d3566314f43e80509434d8
Ammon Roberts <ammoneroberts@gmail.com>
2021-01-25 22:40:51 -0700

    Revert "Revert "Revert "Revert "updated account and settings page and added bootstrap""""
    This reverts commit f99a774c1f0d088a769cd8bf0783b4d04574c444.

--- src/Components/Settings.js
+++ src/Components/Settings.js
@@ -3,44 +3,16 @@ import './css/Settings.css'
 //begin: json-server
 import Api, { db } from '../API';
 //end: json-server
-const userInfo = JSON.parse(localStorage.getItem("user"));
-//console.log(localStorage.getItem("user"));
-class SettingsForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: '' };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          First Name:
-          <input type="text" value={this.props.firstName} onChange={this.handleChange} />
-        </label>
-        <label>
-          Last Name:
-          <input type="text" value={this.props.lastName} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
-  }
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
-}
+
 
 function Settings(props) {
-  const element = <SettingsForm firstName={userInfo.firstName} lastName={userInfo.lastName} />;
+  const userInfo = JSON.parse(localStorage.getItem("user"));
+  const userId = userInfo.id;
+  const user = db.endpoints.Teachers.getOne(userId);
+  console.log(user);
   return (
     <div>
-      {element}
+      Coming Soon
     </div>
   );
 }
